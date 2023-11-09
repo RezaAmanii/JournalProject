db640bf98aece8bd1e774a20a1600efe17fa8cb4
Pdoxxy <117639216+Pdoxxy@users.noreply.github.com>
2023-11-09 12:11:59 +0100

    Adding some test cases

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,25 @@
+import org.group12.model.Task;
+import org.group12.model.TodoList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class MainTest {
-    //Testar
+
+    private final TodoList todoList = new TodoList();
+
+    @Test
+    void addTask(){
+        todoList.setTitle("Exempel Lista");
+        todoList.addTask(new Task("Skriva rapport"));
+    }
+    @Test
+    void addEmptyTitle(){
+        assertThrows(IllegalArgumentException.class, () -> todoList.setTitle(""));
+    }
+    @Test
+    void addEmptyTask(){
+        assertThrows(IllegalArgumentException.class, () -> todoList.addTask(new Task("")));
+    }
 
 }
