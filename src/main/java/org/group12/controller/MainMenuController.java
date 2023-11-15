18960e6d8d2260604f718d4800eb4a69933eae8f
MhdJamal <jamalbasal35@gmail.com>
2023-11-15 19:52:54 +0100

    Added Main Menu Controller

---
+++ src/main/java/org/group12/controller/MainMenuController.java
@@ -0,0 +1,31 @@
+package org.group12.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+
+    public VBox sideBar;
+    public Pane homePane;
+    public Label homeLBL;
+    public Pane journalPane;
+    public Label journalLBL;
+    public Pane todoPane;
+    public Label todoLBL;
+    public Pane calendarPane;
+    public Label calendarLBL;
+    public Pane settingsPane;
+    public Label settingsLBL;
+    public Label nameLBL;
+    public Label homeLBL1;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
