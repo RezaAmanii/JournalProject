3116b82339f1c96f40ac87f7ff9da15d684258c7
Pdoxxy <117639216+Pdoxxy@users.noreply.github.com>
2023-11-21 12:45:11 +0100

    Added contracts to JournalFactory, JournalIDFactory and JournalEntryIDFactory

--- src/main/java/org/group12/model/journal/JournalFactory.java
+++ src/main/java/org/group12/model/journal/JournalFactory.java
@@ -1,13 +1,28 @@
 package org.group12.model.journal;
 
 import org.group12.model.IDFactory;
-
+/**
+ * Represents a factory for creating Journal objects.
+ * This class uses a JournalIDFactory to generate unique IDs for each Journal.
+ */
 public class JournalFactory {
     private JournalIDFactory idFactory;
 
+    /**
+     * Constructs a new JournalFactory.
+     * Initializes the JournalIDFactory used to generate IDs.
+     */
     public JournalFactory() {
         this.idFactory = new JournalIDFactory();
     }
+
+    /**
+     * Creates a new Journal with a unique ID and the given title.
+     *
+     * @param title the title of the Journal
+     * @param entryFactory the factory to create JournalEntry objects
+     * @return the created Journal
+     */
     public Journal createJournal(String title, JournalEntryFactory entryFactory) {
         String ID = idFactory.generateID();
         return new Journal(ID, title, entryFactory);
