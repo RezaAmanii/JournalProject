dc1a2785f4d4c1ea00a6f6167d222ae6ec762efc
Josef.R <55584458+josefir98@users.noreply.github.com>
2023-11-22 13:58:16 +0100

    Created a Container class that holds todo, calender and journal. And has a hashmap that hold all items.

---
+++ src/main/java/org/group12/model/Container.java
@@ -0,0 +1,22 @@
+package org.group12.model;
+
+import org.group12.model.Calendar.Calendar;
+import org.group12.model.journal.Journal;
+import org.group12.model.todo.TodoCollection;
+
+import java.util.HashMap;
+
+public class Container {
+
+    private Calendar calender;
+    private Journal journal;
+    private TodoCollection todoCollection;
+    private HashMap<String, INameable> itemMap;
+
+    public Container() {
+        this.calender = new Calendar();
+        //this.journal = new Journal();
+        this.todoCollection = new TodoCollection();
+        this.itemMap = new HashMap<>();;
+    }
+}
