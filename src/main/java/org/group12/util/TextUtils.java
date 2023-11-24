6cc7eb708fae36d59559a7c51974a38d16e00abf
Pdoxxy <117639216+Pdoxxy@users.noreply.github.com>
2023-11-24 13:10:41 +0100

    Updated for the scenario that text is null

--- src/main/java/org/group12/util/TextUtils.java
+++ src/main/java/org/group12/util/TextUtils.java
@@ -9,9 +9,12 @@ public class TextUtils {
      * Counts the number of words in the given text.
      *
      * @param text the text to count words in
-     * @return the number of words in the text
+     * @return the number of words in the text, or 0 if the text is null
      */
     public static int getWordCount(String text) {
+        if (text == null) {
+            return 0;
+        }
         return text.split("\\s+").length;
     }
 
@@ -20,9 +23,12 @@ public class TextUtils {
      *
      * @param text the text to extract words from
      * @param n the number of words to return
-     * @return the first n words of the text
+     * @return the first n words of the text, or an empty string if the text is null
      */
     public static String getFirstNWords(String text, int n) {
+        if (text == null) {
+            return "";
+        }
         List<String> words = Arrays.asList(text.split("\\s+"));
         return String.join(" ", words.subList(0, Math.min(n, words.size())));
     }
