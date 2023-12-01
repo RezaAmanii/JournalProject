aaa6c2de820ec4114ac409ea68bb3e291787b068
Pdoxxy <117639216+Pdoxxy@users.noreply.github.com>
2023-12-01 10:52:00 +0100

    To ensure that noone by mistake creates a second instance of a IDFactory ,which would reset the counter for that object type, I implemented the Singleton pattern.
    Also edited the classes that called for New ObjecttypeIDFactory() to instead call for IDFactory.getInstance(#OBJECT#IdFactory.class);
    Also added the singleton pattern to the JournalFactory and JournalEntryFactory
    Updated the contracts to match the new functions

--- src/main/java/org/group12/model/IDFactory.java
+++ src/main/java/org/group12/model/IDFactory.java
@@ -29,7 +29,7 @@ public abstract class IDFactory {
                 try {
                     return key.getDeclaredConstructor().newInstance();
                 } catch (Exception e) {
-                    throw new RuntimeException(e);
+                    throw new RuntimeException("Error creating instance of " + key.getName(), e);
                 }
             }));
         }
