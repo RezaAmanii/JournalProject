9237f9b6fbec01050866c460860d031c940fe252
Josef.R <55584458+josefir98@users.noreply.github.com>
2023-12-08 13:26:08 +0100

    Created BigTaskCard

---
+++ src/main/java/org/group12/view/cards/BigTaskCard.java
@@ -0,0 +1,94 @@
+package org.group12.view.cards;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import org.group12.controller.TaskController;
+import org.group12.model.INameable;
+import org.group12.model.todo.IBigTask;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class BigTaskCard extends AnchorPane {
+    // TODO: hur ska items hanteras? här, I en todoPage?, ska vi casta här, ska det vara INameable?
+    private final String ID;
+    private final HashMap<String, INameable> items;
+    private final TaskController taskController;
+    @FXML
+    private Label titleLabel;
+    @FXML
+    private Label dueDateLabel;
+    @FXML
+    private CheckBox statusCheckBox;
+    @FXML
+    private ImageView favouriteImageView;
+
+    public BigTaskCard(String ID, HashMap<String, INameable> items, TaskController taskController){
+        this.items = items;
+        this.ID = ID;
+        this.taskController = taskController;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("bigTaskCard.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        update();
+    }
+
+    @FXML
+    private void cardClicked() {
+
+    }
+
+    @FXML
+    private void imageViewClicked() {
+
+    }
+
+    // TODO: hur funkar taskController?
+    @FXML
+    private void checkBoxToggled() {
+        boolean isSelected = statusCheckBox.isSelected();
+        //taskController.handleSetStatus(ID, isSelected);
+    }
+
+    // TODO: protection måste läggas till
+    public void update() {
+        // get the BigTask to get the information from
+        IBigTask bigTask = (IBigTask) items.get(ID);
+
+        // Set the Title
+        String title = bigTask.getTitle();
+        titleLabel.setText(title);
+
+        // TODO: hur ska datumet formateras?
+        // Set the due date
+        String dueDate = bigTask.getDueDate().toString();
+        dueDateLabel.setText(dueDate);
+
+        // Set the status
+        boolean isCompleted = bigTask.getStatus();
+        statusCheckBox.setSelected(isCompleted);
+
+        // Set the if favourite or not
+        boolean isFavourite = bigTask.isFavourite();
+        // TODO: what happens where depends on Jamals implementation
+        if (isFavourite) {
+            //favouriteImageView.
+        } else {
+            //favouriteImageView.
+        }
+    }
+
+    public String getID() {
+        return ID;
+    }
+}
