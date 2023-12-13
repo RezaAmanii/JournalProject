2af92010bd49d5fa016f60822c200bf1914eca32
MhdJamal <jamalbasal35@gmail.com>
2023-12-13 14:13:13 +0100

    Added  SettingController that allows users to change the color of the Setting window.

---
+++ src/main/java/org/group12/controllerView/SettingsController.java
@@ -0,0 +1,77 @@
+package org.group12.controllerView;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import org.group12.Main;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class SettingsController implements Initializable {
+
+    private static String settings = "/org/group12/view/settingsPage.fxml";
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void darkMode(MouseEvent mouseEvent) {
+        changeWindosColor("my-1","my-2");
+    }
+    public void lightMode(MouseEvent mouseEvent) {
+        changeWindosColor("my-2","my-1");
+
+    }
+
+    private void changeWindosColor(String color1,String color2)
+    {
+        try {
+
+            List<String> fxmlPaths =List.of(settings);
+
+            for(String filePath : fxmlPaths) {
+                InputStream inputStream = getClass().getResourceAsStream(filePath);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder content = new StringBuilder();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                reader.close();
+
+                String modifiedContent = content.toString().replaceAll(color1, color2);
+
+                // Write the modified content to a temporary file
+                File tempFile = File.createTempFile("temp", ".fxml");
+                BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+                writer.write(modifiedContent);
+                writer.close();
+
+                // Replace the original file with the modified content
+                Path originalFilePath = Paths.get(getClass().getResource(filePath).toURI());
+                Files.delete(originalFilePath);
+                Files.move(tempFile.toPath(), originalFilePath);
+
+            }
+
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
