6e267eb3f81d706236e17a24f4b2adb4ab8ec5fe
MhdJamal <jamalbasal35@gmail.com>
2023-12-15 04:11:53 +0100

    Added JavaDoc documentation and deleted unused code

--- src/main/java/org/group12/model/Calendar/Calendar.java
+++ src/main/java/org/group12/model/Calendar/Calendar.java
@@ -66,7 +66,6 @@ public class Calendar implements IObservable, ICalendar, Serializable {
 
     @Override
     public void addEvent(IEvent event) {
-
     }
 
     /**
@@ -82,14 +81,24 @@ public class Calendar implements IObservable, ICalendar, Serializable {
 //        notifyObservers();
     }
 
+    /**
+     * Removes an event with the specified event ID from the event list.
+     *
+     * @param eventId The ID of the event to be removed.
+     */
     public void removeEvent(String eventId){
         eventList.removeIf(ev -> eventId.equals(ev.getID()));
         isEmpty = eventList.isEmpty();
         notifyObservers();
         items.removeItem(eventId);
-//        notifyObservers();
     }
 
+    /**
+     * Retrieves a list of events within the specified calendar week.
+     *
+     * @param week The CalendarWeek representing the week for which to retrieve events.
+     * @return The list of events within the specified week, sorted by date.
+     */
     public List<IEvent> getEventList(CalendarWeek week){
         return eventList.stream()
                 .filter(week::isEventInThisWeek)
@@ -182,6 +191,12 @@ public class Calendar implements IObservable, ICalendar, Serializable {
         }
     }
 
+    /**
+     * Retrieves the event with the specified event ID.
+     *
+     * @param eventId The ID of the event to retrieve.
+     * @return The event with the specified ID, or null if not found.
+     */
     public IEvent getEvent(String eventId) {
         return this.eventList.stream().filter(ev -> Objects.equals(ev.getID(), eventId)).findFirst().orElse(null);
     }
--- src/main/java/org/group12/view/CalendarView.java
+++ src/main/java/org/group12/view/CalendarView.java
@@ -1,7 +1,6 @@
 package org.group12.view;
 
-import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import org.group12.controller.WeekCalendarController;
@@ -18,20 +17,43 @@ import java.util.function.Function;
 
 import static org.group12.util.Globals.loadFxml;
 
+/**
+ * The CalendarView class represents the view for displaying a calendar.
+ */
 public class CalendarView {
 
     private BorderPane calendarPane;
+
+    /**
+     * Constructs a CalendarView object with the specified calendar pane.
+     *
+     * @param calendarPane The BorderPane representing the calendar pane.
+     */
     public CalendarView(BorderPane calendarPane) {
         this.calendarPane = calendarPane;
 
     }
 
+    /**
+     * Draws the calendar view for the specified week and event list.
+     *
+     * @param week              The CalendarWeek representing the week to be displayed.
+     * @param eventList         The list of IEvent objects representing the events to be displayed.
+     * @param deleteEventAction The action to perform when deleting an event.
+     * @param getEventFn        The function to retrieve an event.
+     */
     public void drawCalendar(CalendarWeek week , List<IEvent> eventList, Consumer<String> deleteEventAction, Function<String, IEvent> getEventFn) {
         Globals.FxmlUi<GridPane, WeekCalendarController> form = loadFxml("/org/group12/view/weekCalendar.fxml");
         form.getController().initialize(week, eventList, deleteEventAction, getEventFn);
         calendarPane.setCenter(form.getRoot());
     }
 
+    /**
+     * Opens the "New Event" form and initializes it with the onSaveAction consumer.
+     *
+     * @param onSaveAction The action to perform when saving a new event.
+     * @throws IOException If an I/O error occurs while opening the form.
+     */
     public void onAddEvent(Consumer<EventData> onSaveAction) throws IOException {
         NewEventView controller =  Globals.openNewForm("/org/group12/view/newEvent.fxml", "New Event", false);
         controller._initialize(onSaveAction);
